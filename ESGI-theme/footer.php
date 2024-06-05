48fc23bd1d8298825c4fe51740c12a0df8ab7acb
Alice Lemerre <al.lemerre@laposte.net>
2024-06-05 18:02:49 +0200

    initialisation du thème, création des components de base et de leur styles

---
+++ ESGI-theme/footer.php
@@ -0,0 +1,9 @@
+        <footer id="site-footer">
+            <div class="container">
+                © <?= date('Y') ?> Edouard Sombié
+            </div>
+        </footer>
+        <?php wp_footer() ?>
+        </body>
+
+        </html>
